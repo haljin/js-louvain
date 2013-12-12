1fb9d7fe2e620ade7aca46420376685ee7de887e
Pawel Antemijczuk <paul.shinobi@gmail.com>
2013-12-12 20:47:03 +0100

    Working louvain!

--- louvain.js
+++ louvain.js
@@ -6,77 +6,83 @@
 *                                                                       *
 ************************************************************************/
 var Graph = (function () {
+
     function Graph(inNodes, edges) {
-        this.nodes = {};
+        this._nodes = {};
 
         for (var i = 0; i < inNodes.length; i++) {
-           this.nodes[inNodes[i]] = {};
+           this._nodes[inNodes[i]] = {};
         }
         for (var i = 0; i < edges.length; i++) {
-            this.nodes[edges[i].a][edges[i].b] = { weight: edges[i].score };
-            this.nodes[edges[i].b][edges[i].a] = { weight: edges[i].score };
+            this._nodes[edges[i].a][edges[i].b] = { weight: edges[i].score };
+            this._nodes[edges[i].b][edges[i].a] = { weight: edges[i].score };
         }
 
     };
 
     Graph.prototype.copy = function (newGraph) {
-        for (node in this.nodes) {
+        for (node in this._nodes) {
             newGraph.add_node(node);
-            for (nb in this.nodes[node]) {
+            for (nb in this._nodes[node]) {
                 if (!newGraph.get_node(nb))
                     newGraph.add_node(nb);                    
-                newGraph.add_edge(node, nb, this.nodes[node][nb].weight);
+                newGraph.add_edge(node, nb, this._nodes[node][nb].weight);
             }
         }
 
     };
 
     Graph.prototype.clear = function () {
-        this.nodes = {};
+        this._nodes = {};
     };
 
     Graph.prototype.add_node = function (node) {
-        if (!this.nodes[node])
-            this.nodes[node] = {};
+        if (!this._nodes[node])
+            this._nodes[node] = {};
     };
 
     Graph.prototype.add_edge = function (a, b, score) {
-        this.nodes[a][b] = this.nodes[b][a] = { weight: score };
+        if (this._nodes[a] === undefined)
+            this.add_node(a);
+        if (this._nodes[b] === undefined)
+            this.add_node(b);
+
+        this._nodes[a][b] = this._nodes[b][a] = { weight: score };
     };
 
     Graph.prototype.remove_node = function (node) {
-        for (n in this.nodes)
-            delete this.nodes[n][node];
-        delete this.nodes[node];
+        for (n in this._nodes)
+            delete this._nodes[n][node];
+        delete this._nodes[node];
     };
 
     Graph.prototype.remove_edge = function (a, b) {
-        delete this.nodes[a][b];
-        delete this.nodes[b][a];
+        delete this._nodes[a][b];
+        delete this._nodes[b][a];
     };
 
     Graph.prototype.get_edge = function (a, b) {
-        return this.nodes[a][b];
+        if (this._nodes[a] !== undefined)
+            return this._nodes[a][b];
+        else
+            return undefined;
     };
 
     Graph.prototype.get_node = function (node) {
-        return this.nodes[node];
+        return this._nodes[node];
     };
 
     Graph.prototype.get_nodes = function () {
-        var result = [];
-        for (node in this.nodes)
-            result.push(node);
-        return result;
+        return this._nodes;
     };
 
     Graph.prototype.get_edges = function () {
         var result = [];
         var visited = {};
-        for (node in this.nodes)
-            for (nb in this.nodes[node])
+        for (node in this._nodes)
+            for (nb in this._nodes[node])
                 if (!visited[nb + node]) {
-                    result.push({ a: node, b: nb, data: this.nodes[node][nb] });
+                    result.push({ a: node, b: nb, data: this._nodes[node][nb] });
                     visited[node + nb] = true;
                 }
         return result;
@@ -92,7 +98,10 @@ var Graph = (function () {
 
     Graph.prototype.degree = function (node, weighted) {
         var deg = 0;
-        for (nb in this.nodes[node]) weighted ? deg += this.nodes[node][nb].weight : deg++;
+        for (nb in this._nodes[node]){
+            var this_deg = weighted ? this._nodes[node][nb].weight : 1;
+            deg += (nb === node) ? this_deg*2 : this_deg;
+        }
         return deg;
     };
 
@@ -100,7 +109,7 @@ var Graph = (function () {
 })();
 
 /************************************************************************
-*       CLASS: louvain                                                  *
+*       MODULE: louvain                                                  *
 *       Based on Python code by Thomas Aynaud <thomas.aynaud@lip6.fr>   *
 *       Implements Louvain method of community detection in graphs      *
 *                                                                       *
@@ -119,7 +128,7 @@ var louvain = function () {
     var communities_at_level = function (dendro, level) {
         var partition = {}
         for (keys in dendro[0])
-            partition[keys] = null;
+            partition[keys] = dendro[0][keys];
 
         for (var i = 1; i < level + 1; i++)
             for (node in partition)
@@ -138,12 +147,12 @@ var louvain = function () {
         if (graph.edge_count() == 0) {
             var part = {};
             var nodes = graph.get_nodes();
-            for (var i = 0; i < nodes.length; i++) 
-                part[graph.get_node(nodes[i])] = graph.get_node(nodes[i]);
+            for(node in nodes)
+                part[node] = node;
             return part;
         }
 
-        var curr_graph = new Graph();
+        var curr_graph = new Graph([],[]);
         graph.copy(curr_graph);
 
         __init_state(curr_graph);
@@ -175,15 +184,15 @@ var louvain = function () {
     };
 
     var induced_graph = function (partition, graph) {
-        var ret = new Graph();
+        var ret = new Graph([],[]);
         for (key in partition)
             ret.add_node(partition[key]);
 
         var edges = graph.get_edges();
         for (var i = 0; i < edges.length; i++) {
             var weight = edges[i].data.weight;
-            var com1 = partition[a];
-            var com2 = partition[b];
+            var com1 = partition[edges[i].a];
+            var com2 = partition[edges[i].b];
             var e = ret.get_edge(com1, com2);
             var w_prec = e ? e.weight : 0;
             ret.add_edge(com1, com2, w_prec + weight);
@@ -199,8 +208,8 @@ var louvain = function () {
 
         for (key in dict) {
             var value = dict[key];
-            var new_value = new_values[value] || -1;
-            if (new_value == -2) {
+            var new_value = new_values[value] === undefined? -1 : new_values[value];
+            if (new_value == -1) {
                 new_values[value] = count;
                 new_value = count;
                 count = count + 1;
@@ -222,9 +231,8 @@ var louvain = function () {
             modif = false;
             nb_pass_done++;
 
-            var graphnodes = graph.nodes();
-            for (var i = 0; i < graphnodes.length; i++) {
-                var node = graph.get_node[graphnodes[i]];
+            var graphnodes = graph.get_nodes();
+            for (node in graphnodes) {
                 var com_node = _node2com[node];
                 var degc_totw = (_gdegrees[node] || 0) / (_total_weight * 2);
                 var neigh_communities = __neighcom(node, graph);
@@ -239,7 +247,7 @@ var louvain = function () {
                         best_com = com;
                     }
                 }
-                __insert(node, best_com, (neigh_communities[com_node] || 0));
+                __insert(node, best_com, (neigh_communities[best_com] || 0));
                 if (best_com != com_node)
                     modif = true;                
             }
@@ -264,8 +272,8 @@ var louvain = function () {
     };
 
     var __remove = function (node, com, weight) {
-        _degrees[com] = (degrees[com] || 0) - (_gdegrees[node] || 0);
-        _internals[com] = (_internals[com] || 0);
+        _degrees[com] = (_degrees[com] || 0) - (_gdegrees[node] || 0);
+        _internals[com] = (_internals[com] || 0) - weight - (_loops[node] || 0);
         _node2com[node] = -1;
     };
 
@@ -279,13 +287,15 @@ var louvain = function () {
     var __modularity = function () {
         var links = _total_weight;
         var result = 0;
+        var valset = {};
+        for (key in _node2com)
+            valset[_node2com[key]] = {};
 
-        for (key in _node2com) {
-            var community = _node2com[key];
+        for (community in valset) {            
             var in_degree = _internals[community] || 0;
             var degree = _degrees[community] || 0;
             if (links > 0)
-                result = result - (Math.pow(degree / (2 * links), 2));
+                result = result + in_degree/links - (Math.pow(degree / (2 * links), 2));
         }
         return result;
         
@@ -298,22 +308,24 @@ var louvain = function () {
         _total_weight = 0;
         _degrees = {};
         _gdegrees = {};
+        _internals = {};
         var count = 0;
 
         var allnodes = graph.get_nodes();
         var alledges = graph.get_edges();
-        for (var i = 0; i < alledges; i++) {
+        for (var i = 0; i < alledges.length; i++) {
             _total_weight += alledges[i].data.weight;
         };
 
-        for (var i = 0; i < allnodes.length; i++) {
-            var node = allnodes[i];          
-            _node2com[node] = i;
-            var deg = graph.degree(node);
-            _degrees[i] = deg;
-            _gdegrees[i] = deg;
-            _loops[node] = graph.get_edge(node, node).data.weight || 0;
-            _internals[i] = _loops[node];
+        for (node in allnodes) {       
+            _node2com[node] = count;
+            var deg = graph.degree(node, true);
+            _degrees[count] = deg;
+            _gdegrees[node] = deg;
+            var possible_loop = graph.get_edge(node, node);
+            _loops[node] = possible_loop ? possible_loop.weight : 0;
+            _internals[count] = _loops[node];
+            count++;
         }
 
 
